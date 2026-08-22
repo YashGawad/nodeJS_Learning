5db20264cd737dbee15e1be916b8e02a5cc3004e
Yash Gawad <krutikagawad5@gmail.com>
2026-08-22 22:10:09 +0530

    Used exit method

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  console.log(req.url, req.method, req.headers);
+  process.exit();
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-  console.log(`Server is running at port:${PORT}`);
+  console.log(`Server is running at http://localhost:3000`);
 });
