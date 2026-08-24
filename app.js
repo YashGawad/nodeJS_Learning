8924f1d2046d5e4f38e34fd5f4eafceb68384257
Yash Gawad <krutikagawad5@gmail.com>
2026-08-24 18:09:37 +0530

    Learned to give resposne

--- app.js
+++ app.js
@@ -2,7 +2,26 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  process.exit();
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<HTML>");
+    res.write("<Head><Title>nodeJS</Title></Head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</HTML>");
+    return res.end();
+  } else if (req.url === "/products") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<HTML>");
+    res.write("<Head><Title>nodeJS</Title></Head>");
+    res.write("<body><h1>This are the products</h1></body>");
+    res.write("</HTML>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<HTML>");
+  res.write("<Head><Title>nodeJS</Title></Head>");
+  res.write("<body><h1>Learning NodeJS</h1></body>");
+  res.write("</HTML>");
 });
 
 const PORT = 3000;
