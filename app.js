5f7cd525424168e596ec3f18ccd6b715e26af42b
Yash Gawad <krutikagawad5@gmail.com>
2026-08-24 18:36:01 +0530

    Get input from user

--- app.js
+++ app.js
@@ -6,14 +6,19 @@ const server = http.createServer((req, res) => {
     res.setHeader("Content-Type", "text/html");
     res.write("<HTML>");
     res.write("<Head><Title>nodeJS</Title></Head>");
-    res.write("<body><h1>Welcome Home</h1></body>");
-    res.write("</HTML>");
-    return res.end();
-  } else if (req.url === "/products") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<HTML>");
-    res.write("<Head><Title>nodeJS</Title></Head>");
-    res.write("<body><h1>This are the products</h1></body>");
+    res.write("<body><h1>Enter your details : </h1></body>");
+    res.write('<form action="submit-details" method="POST">');
+    res.write(
+      '<input type="text" name="username" placeholder="Enter your name"><br>',
+    );
+    res.write('<input type="radio" id="male" name="gender" value="male">');
+    res.write('<label for="male">Male</label>');
+    res.write(
+      '<input type="radio" id="female" name="gender" value="female">',
+    );
+    res.write('<label for="female">Female</label><br>');
+    res.write('submit <input type="submit" value="Submit">');
+    res.write("</form>");
     res.write("</HTML>");
     return res.end();
   }
