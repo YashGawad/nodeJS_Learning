4b85baddec226f80f6ef689cfdfb6b2224e4638e
Yash Gawad <krutikagawad5@gmail.com>
2026-08-30 11:56:16 +0530

    Created userRequestHandler module and imported from user.js ro  app.jsx

--- app.js
+++ app.js
@@ -1,61 +1,9 @@
 const http = require("http");
-const fs = require("fs");
+const requestHandler = require("./user");
 
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method, req.headers);
-  if (req.url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<HTML>");
-    res.write("<Head><Title>nodeJS</Title></Head>");
-    res.write("<body><h1>Enter your details : </h1></body>");
-    res.write('<form action="submit-details" method="POST">');
-    res.write(
-      '<input type="text" name="username" placeholder="Enter your name"><br>',
-    );
-    res.write('<input type="radio" id="male" name="gender" value="male">');
-    res.write('<label for="male">Male</label>');
-    res.write('<input type="radio" id="female" name="gender" value="female">');
-    res.write('<label for="female">Female</label><br>');
-    res.write('<input type="submit" value="Submit">');
-    res.write("</form>");
-    res.write("</HTML>");
-    return res.end();
-  } else if (
-    req.url.toLocaleLowerCase() === "/submit-details" &&
-    req.method === "POST"
-  ) {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
+const server = http.createServer(requestHandler);
 
-    req.on("end", () => {
-      const fullBody = Buffer.concat(body).toString();
-      console.log(fullBody);
-
-      const params = new URLSearchParams(fullBody);
-      // const bodyObject = {};
-      // for (const [key, val] of params.entries()) {
-      //   bodyObject[key] = val;
-      // }
-      const bodyObject = Object.fromEntries(params);
-      console.log(bodyObject);
-      fs.writeFileSync("user.txt", JSON.stringify(bodyObject));
-    });
-
-    fs.writeFileSync("user.txt", "Yash Gawad");
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<HTML>");
-  res.write("<Head><Title>nodeJS</Title></Head>");
-  res.write("<body><h1>Learning nodeJS and creating projects</h1></body>");
-  res.write("</HTML>");
-});
-
-const PORT = 3000;
-server.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:3000`);
-});
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+})
