ce22afbe68a235be2589cd105fecc7ffeb7f39fc
Yash Gawad <krutikagawad5@gmail.com>
2026-09-07 22:48:04 +0530

    Changed the port

--- practice1.js
+++ practice1.js
@@ -78,7 +78,7 @@ const server = http.createServer((req, res) => {
   }
 });
 
-const PORT = 3003;
+const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
